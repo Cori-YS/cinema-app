2e1f09a525c90cf5bfc0c43c4f2f17d193e4eccd
Cori-YS <ismeliocori@hotmail.com>
2025-05-06 16:23:26 +0100

    feat: create movies component

---
+++ src/components/moviesList/style.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  card: {
+    width: 215,
+    marginRight: 16,
+    marginTop: 10,
+    alignItems: 'center',
+  },
+  poster: {
+    width: '100%',
+    height: 310,
+    borderRadius: 20,
+    marginBottom: 8,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#fff',
+  },
+  subtitle: {
+    fontSize: 12,
+    color: '#aaa',
+  },
+});
